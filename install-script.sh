e691a0665cc4dfd65ad5534ac16d1b9f454b7979
Today <happybobtm@protonmail.com>
2020-08-18 18:50:51 -0300

    commit

--- install-script.sh
+++ install-script.sh
@@ -1,7 +1,7 @@
 #dependencies: git, yay (AUR helper), multilib enabled (edit /etc/pacman.conf).
 #warning! decline the GPG/PGP import from spotify
 
-#install (pacman)
+#Install (pacman)
 sudo pacman -S pavucontrol xrandr dmenu graphviz grub-customizer lilypond imagemagick source-highlight dblatex fop w3m lynx xorg-xwininfo feh playerctl flameshot i3-gaps firefox code alacritty lutris pcmanfm gimp bashtop macchanger steam figlet lolcat neofetch xarchiver lib32-mesa lxappearance gnome-boxes gnome-keyring wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader meson systemd dbus gnome-keyring libgnome-keyring -y
 
 #Nvidia
@@ -10,32 +10,32 @@ sudo pacman -S pavucontrol xrandr dmenu graphviz grub-customizer lilypond imagem
 #AMD (Radeon) 
 #sudo pacman -S vulkan-radeon lib32-vulkan-radeon -y
 
-#figlet
+#Figlet
 figlet Spotify | lolcat
 
-#spotify 
+#Spotify 
 curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
 
-#figlet
+#Figlet
 figlet Yay | lolcat
 
-#echo
+#Echo
 echo warning! decline the GPG/PGP import from spotify
 
-#install (yay)
+#Install (yay)
 yay -S timeshift lbry lightdm-webkit-theme-aether nerd-fonts-mononoki brave-nightly-bin simplenote discord-canary simplenote cmatrix-git toilet betterdiscordctl-git spotify spicetify-cli picom-tryone-git -y
 
-#figlet
+#Figlet
 figlet Mac Address Script | lolcat
 
-#mac address script
+#Mac address script
 cd ~
 git clone https://github.com/happybobtm/mac-address.git
 cd ~/mac-address
 mv ~/mac-address/mac-address.sh ~/
 cd ~
 
-#figlet
+#Figlet
 figlet i3 | lolcat
 
 #i3-gaps config
@@ -46,16 +46,16 @@ mkdir ~/.config/i3
 mv ~/i3-today-dark-config/config ~/.config/i3
 cd ~
 
-#figlet
+#Figlet
 figlet Bumbleebee Status | lolcat
 
-#bumblebee-status
+#Bumblebee-status
 cd ~
 git clone https://github.com/tobi-wan-kenobi/bumblebee-status.git
 mv ~/bumblebee-status ~/.config
 cd ~
 
-#bumblebee-modules
+#Bumblebee-modules
 cd ~
 git clone https://github.com/happybobtm/bumblebee-modules.git
 cd ~/.config/bumblebee-status/bumblebee_status/modules/contrib/ 
@@ -67,36 +67,36 @@ mv weather.py arch-update.py kernel.py playerctl.py ~/.config/bumblebee-status/b
 mv time.py date.py memory.py ~/.config/bumblebee-status/bumblebee_status/modules/core/
 cd ~
 
-#figlet
+#Figlet
 figlet BetterDiscord | lolcat
 
 #BetterDiscord
 betterdiscordctl install -f discord-canary
 
-#betterdiscord plugins
+#Betterdiscord plugins
 cd ~
 git clone https://github.com/happybobtm/betterdiscord-plugins.git
 cp -a ~/betterdiscord-plugins ~/.config/BetterDiscord/plugins
 cd ~
 
-#betterdiscord themes
+#Betterdiscord themes
 cd ~
 git clone https://github.com/happybobtm/betterdiscord-theme.git
 cp -a ~/betterdiscord-theme ~/.config/BetterDiscord/themes
 cd ~
 
-#figlet
+#Figlet
 figlet Wallpaper | lolcat
 
-#wallpaper
+#Wallpaper
 cd ~
 git clone https://github.com/happybobtm/wallpaper.git
 cd ~
 
-#figlet
+#Figlet
 figlet Neofetch | lolcat
 
-#neofetch
+#Neofetch
 cd ~
 neofetch
 git clone https://github.com/happybobtm/neofetch.git
@@ -104,30 +104,30 @@ rm -rf ~/.config/neofetch/config.conf
 mv ~/neofetch/config.conf ~/.config/neofetch
 cd ~
 
-#figlet
+#Figlet
 figlet Alacritty | lolcat
 
-#alacritty
+#Alacritty
 cd ~
 mkdir ~/.config/alacritty
 git clone https://github.com/happybobtm/alacritty.git
 mv ~/alacritty/alacritty.yml ~/.config/alacritty
 cd ~
 
-#figlet
+#Figlet
 figlet .Bahsrc | lolcat
 
-#bashrc
+#Bashrc
 cd ~
 rm -rf .bashrc
 mv ~/bashrc/.bashrc ~
 git clone https://github.com/happybobtm/.bahsrc.git
 cd ~
 
-#figlet
+#Figlet
 figlet Spicetify | lolcat
 
-#spicetify
+#Spicetify
 cd ~
 git clone https://github.com/happybobtm/spicetify.git
 sudo chmod a+wr /opt/spotify
@@ -144,7 +144,7 @@ cd ~
 spicetify backup apply
 cd ~
 
-#figlet
+#Figlet
 figlet GTK | lolcat
 
 #GTK
@@ -155,7 +155,7 @@ cp -a ~/gtk ~/.themes
 sudo cp -a ~/gtk /usr/share/themes/
 cd ~
 
-#figlet
+#Figlet
 figlet Icons | lolcat
 
 #Icons
@@ -166,7 +166,7 @@ cp -a ~/cursor ~/.icons
 sudo cp -a ~/icons /usr/share/icons/
 cd ~
 
-#figlet
+#Figlet
 figlet Cursor | lolcat
 
 #Cursor
@@ -176,7 +176,7 @@ cp -a ~/cursor ~/.icons
 sudo cp -a ~/cursor /usr/share/icons/
 cd ~
 
-#figlet
+#Figlet
 figlet Grub | lolcat
 
 #Grub
@@ -184,13 +184,13 @@ cd ~
  wget -O - https://github.com/shvchk/fallout-grub-theme/raw/master/install.sh | bash
 cd ~
 
-#figlet
+#Figlet
 figlet gamemode | lolcat
 
-#echo
+#Echo
 echo pay atention!
 
-#gamemode
+#Gamemode
 cd ~
 git clone https://github.com/FeralInteractive/gamemode.git
 cd gamemode
@@ -198,10 +198,10 @@ git checkout 1.5.1 # omit to build the master branch
 ./bootstrap.sh
 cd ~
 
-#figlet
+#Figlet
 figlet Proton | lolcat
 
-#echo
+#Echo
 echo pay atention!
 
 #Custom Proton
@@ -211,11 +211,11 @@ sudo chmod +x cproton.sh
 ./cproton.sh
 cd ~
 
-#figlet
+#Figlet
 figlet lightdm | lolcat
 
-#enable lightdm
+#Enable lightdm
 sudo systemctl enable lightdm.service
 
-#start lightdm 
+#Start lightdm 
 sudo systemctl start lightdm.service
